44e8abb1df3c9fbfca9f1bcf0aca726f9742b1bf
mlnvuser <thebrilliantplayer@gmail.com>
2026-05-11 17:13:13 +0300

    Добавление новых переменных в .env

--- KarelianHarvest/settings.py
+++ KarelianHarvest/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
